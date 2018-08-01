626c33e8a0e84df34858aa031059ab76a5c48db7
unwarysage <unwary@comcast.net>
2018-08-01 14:17:58 -0500

    add crude but working message-based error object catching

--- src/babel/middleware.clj
+++ src/babel/middleware.clj
@@ -1,9 +1,9 @@
 (ns babel.middleware
   (:require [babel.processor :as processor]
-            [clojure.tools.nrepl.middleware])
-  (:import clojure.tools.nrepl.transport.Transport))
+            [clojure.tools.nrepl.middleware]
+            [clojure.tools.nrepl.transport :as t]))
 
-(defn interceptor
+#_(defn interceptor-old
   "applies processor/modify-errors to every response that emerges from the server"
   [handler]
   (fn [inp-message]
@@ -14,6 +14,20 @@
                         (recv [this timeout] (.recv transport timeout))
                         (send [this msg]     (.send transport (processor/modify-errors msg)))))))))
 
+
+(defn interceptor
+  "applies proccesor/modify-errors to every error object that emerges from the server"
+  [handler]
+  (fn [inp-message]
+    (let [{:keys [session transport]} inp-message]
+    (if-let [e (@session #'*e)]
+      (do
+        (when (get @session #'*e) (println (get @session #'*e)))
+        (handler (assoc inp-message :err (str "Babel here:" (.getMessage e)))))
+      (handler (assoc inp-message :message "Babel wuz here"))))))
+
 ;;sets the appropriate flags on the middleware so it is placed correctly
 (clojure.tools.nrepl.middleware/set-descriptor! #'interceptor
                                                 {:expects #{"eval"} :requires #{} :handles {}})
+
+(println "babel.middleware loaded")
