5e4ccc90fcd67e92c7b07a5a4ac23090d944f5d5
unwarysage <unwary@comcast.net>
2018-08-01 17:09:08 -0500

    move object access to post eval stage
    unsure of what extract-error actually returns as it doesn't respond to class or str in comprehensible ways.

--- src/babel/middleware.clj
+++ src/babel/middleware.clj
@@ -1,30 +1,19 @@
 (ns babel.middleware
   (:require [babel.processor :as processor]
             [clojure.tools.nrepl.middleware]
-            [clojure.tools.nrepl.transport :as t]))
+            [clojure.tools.nrepl.transport :as t])
+  (:use [clojure.tools.nrepl.misc :only (response-for)]))
 
-#_(defn interceptor-old
+(defn interceptor
   "applies processor/modify-errors to every response that emerges from the server"
   [handler]
   (fn [inp-message]
     (let [transport (inp-message :transport)]
       (handler (assoc inp-message :transport
-                      (reify Transport
+                      (reify t/Transport
                         (recv [this] (.recv transport))
                         (recv [this timeout] (.recv transport timeout))
                         (send [this msg]     (.send transport (processor/modify-errors msg)))))))))
-
-
-(defn interceptor
-  "applies proccesor/modify-errors to every error object that emerges from the server"
-  [handler]
-  (fn [inp-message]
-    (let [{:keys [session transport]} inp-message]
-    (if-let [e (@session #'*e)]
-      (do
-        (when (get @session #'*e) (println (get @session #'*e)))
-        (handler (assoc inp-message :err (str "Babel here:" (.getMessage e)))))
-      (handler (assoc inp-message :message "Babel wuz here"))))))
 
 ;;sets the appropriate flags on the middleware so it is placed correctly
 (clojure.tools.nrepl.middleware/set-descriptor! #'interceptor
--- src/babel/processor.clj
+++ src/babel/processor.clj
@@ -4,12 +4,24 @@
              [errors.messageobj :as m-obj]
              [errors.prettify-exception :as p-exc]))
 
+(defn extract-error "takes an nREPL message and extracts the most recent error object, or nil if the message is not relevant" [inp-message]
+;;if we have both an :op 'eval' message and the exception key exists, we can assume that an error has been caused
+  (let [{:keys [transport op ex session]} inp-message]
+    (if (and (= op "eval") ex)
+      (let [e (get @session #'*e)]
+        (if e e "Something is wrong")
+        nil))))
+
 (defn modify-errors "takes a nREPL response, and returns a message with the errors fixed"
   [inp-message]
   (if (contains? inp-message :err)
-    ;;replace the assoced value with a function call as needed.
-    (assoc inp-message :err (m-obj/get-all-text (:msg-info-obj (p-exc/process-spec-errors (inp-message :err)))))
-    ;(assoc inp-message :err (str (inp-message :err))) ;; Debugging
+    (let [e (extract-error inp-message)]
+      (assoc inp-message :err e))
     inp-message))
+;  (if (contains? inp-message :err)
+;    ;;replace the assoced value with a function call as needed.
+;    (assoc inp-message :err (m-obj/get-all-text (:msg-info-obj (p-exc/process-spec-errors (inp-message :err)))))
+;    ;(assoc inp-message :err (str (inp-message :err))) ;; Debugging
+;    inp-message))
 
 (println "babel.processor loaded")
